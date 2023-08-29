dd714588de80a3f2e7b4b8626092a5c73d82b769
IJTINK7 <ijtink7@mail.ru>
2023-08-29 16:05:29 +0300

    Creating GroceryStore.tsx file

---
+++ src/GroceryStore.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+export const GroceryStore = () => {
+	return (
+		<div>GroceryStore</div>
+	);
+};
+
+
+// export const GroceryStore = () => {
+// 	return (
+// 		<div>
+// 			<div><strong>Fruits:</strong></div>
+// 			<ol>
+// 				<li><input type="checkbox" checked={true}/> <span>name</span>, price: 123 </li>
+// 				<li><input type="checkbox" checked={true}/> <span>name</span>, price: 123 </li>
+// 				<li><input type="checkbox" checked={true}/> <span>name</span>, price: 123 </li>
+// 			</ol>
+// 			<div><strong>Vegetables:</strong></div>
+// 			<ul>
+// 				<li><input type="checkbox" checked={true}/> <span>name</span>, price: 123 </li>
+// 				<li><input type="checkbox" checked={true}/> <span>name</span>, price: 123 </li>
+// 				<li><input type="checkbox" checked={true}/> <span>name</span>, price: 123 </li>
+// 			</ul>
+// 		</div>
+// 	);
+// };
+
+// type PropsType ={
+// 	fruits: FruitsType[]
+// 	vegetables: VegetablesType[]
+// }
+//
+// type FruitsType = {
+// 	fruitName: string
+// 	price: number
+// 	bought: boolean
+// }
+// type VegetablesType = {
+// 	vegetableName: string
+// 	price: number
+// 	bought: boolean
+// }
+// export const GroceryStore = (props: PropsType) => {
+// 	return (
+// 		<div>
+// 			<div><strong>Fruits:</strong></div>
+// 			<ol>
+// 				<li><input type="checkbox" checked={props.fruits[0].bought}/> <span>{props.fruits[0].fruitName}</span>, price: {props.fruits[0].price} </li>
+// 				<li><input type="checkbox" checked={props.fruits[1].bought}/> <span>{props.fruits[1].fruitName}</span>, price: {props.fruits[1].price} </li>
+// 				<li><input type="checkbox" checked={props.fruits[2].bought}/> <span>{props.fruits[2].fruitName}</span>, price: {props.fruits[2].price} </li>
+// 			</ol>
+// 			<div><strong>Vegetables:</strong></div>
+// 			<ul>
+// 				<li><input type="checkbox" checked={props.vegetables[0].bought}/> <span>{props.vegetables[0].vegetableName}</span>, price: {props.vegetables[0].price} </li>
+// 				<li><input type="checkbox" checked={props.vegetables[1].bought}/> <span>{props.vegetables[1].vegetableName}</span>, price: {props.vegetables[1].price} </li>
+// 				<li><input type="checkbox" checked={props.vegetables[2].bought}/> <span>{props.vegetables[2].vegetableName}</span>, price: {props.vegetables[2].price} </li>
+// 			</ul>
+// 		</div>
+// 	);
+// };
