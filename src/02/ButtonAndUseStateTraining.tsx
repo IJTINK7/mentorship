4c32e85ef7f92abb0e69e95060d05f02c1fdc035
IJTINK7 <ijtink7@mail.ru>
2023-09-04 20:02:14 +0300

    Training to use useState with "Names" data

--- src/02/ButtonAndUseStateTraining.tsx
+++ src/02/ButtonAndUseStateTraining.tsx
@@ -29,13 +29,20 @@
 import React, {useState} from 'react';
 
 export const ButtonAndUseStateTraining = () => {
-	let [car, setCar] = useState('lada')
- 	const changeCar = (newCar: string) => {
+
+	let [car, setCar] = useState('Lada')
+	let [name, setName] = useState('Igor')
+
+	const changeCar = (newCar: string) => {
 		setCar(newCar)
 	}
 	const sayHello =()=> {
 		setCar('123')
 	}
+	const changeName =(newName: string)=> {
+		setName(newName)
+	}
+
 	return (
 		<div>
 			<div>
@@ -46,6 +53,14 @@ export const ButtonAndUseStateTraining = () => {
 				<button onClick={()=>changeCar('Ferrari')}>Ferrari</button>
 				<button onClick={sayHello}>Hello</button>
 			</div>
+			<div>
+				<h2>Names</h2>
+				<div>{name}</div>
+				<button onClick={()=>changeName('Alex')}>Alex</button>
+				<button onClick={()=>changeName('Roma')}>Roma</button>
+				<button onClick={()=>changeName('Igor')}>Igor</button>
+			</div>
 		</div>
+
 	);
 };
